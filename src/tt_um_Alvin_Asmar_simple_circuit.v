75c067fad7e62715280f330c59bb48588755962e
alvinkasmar <alvinkasmar@gmail.com>
2025-12-09 22:13:15 -0500

    Update tt_um_Alvin_Asmar_simple_circuit.v

--- src/tt_um_Alvin_Asmar_simple_circuit.v
+++ src/tt_um_Alvin_Asmar_simple_circuit.v
@@ -9,8 +9,7 @@ input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
 output wire [7:0] uio_out, // IOs: Output path
-output wire [7:0] uio_oe, // IOs: Enable path (0=input,
-1=output)
+output wire [7:0] uio_oe, // IOs: Enable path (0=input,1=output)
 input wire ena, // Always 1 when design is powered
 input wire clk, // Clock input
 input wire rst_n // Active-low reset
